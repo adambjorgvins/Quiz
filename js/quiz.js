735f035cee2e1502036c95f98a5b99a3406dab9b
Adam Bæhrenz Björgvinsson <adambjorgvinsson@gmail.com>
2016-03-12 14:35:58 +0000

    n1 * nr2 því sér set myndir í staðinn fyrir tölur

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,16 @@
 /**
  * Þetta er hlutur sem heldur utanum eins spurningu, svarmöguleika og hver möguleikanna er rétta svarið.
- * @param question Þetta er spurningin eins og t.d "3 * 2"
+ * @param nr1 Þetta er fyrsta talan í spurningunni eins og t.d 3 í "3 * 2"
+ * @param nr2 Þetta er seinni talan í spurningunni eins og t.d 2 í "3 * 2"
  * @param answers Þetta eru svarmöguleikarnir eins og t.d [0,5,6]
  * @param rightAnswerIndex Þetta er vísun í rétta svarið eins og t.d 2
  * @constructor
  */
-function Question (question,answers,rightAnswerIndex) {
-    this.question = question
-    this.answere = answers
-    this.rightix = rightAnswerIndex
+function Question (nr1,nr2,answers,rightAnswerIndex) {
+    this.nr1 = nr1
+    this.nr2 = nr2
+    this.answers = answers
+    this.rightAnswerIndex = rightAnswerIndex
 }
 
 /**
@@ -17,19 +19,27 @@ function Question (question,answers,rightAnswerIndex) {
  * @constructor
  */
 function RandomQuestion(){
+    // Búa til tvær random tölur
     var rand1 = getRandomInt(0,10)
     var rand2 = getRandomInt(0,10)
+
+    // Reikna margfeldi talnanna
     var ans = rand1 * rand2
+
+    // Búa til 3 random svör
     var answeres = []
     answeres.push(getRandomInt(0,100))
     answeres.push(getRandomInt(0,100))
     answeres.push(getRandomInt(0,100))
+
+    // Finna random stað fyrir rétta svarið
     var rightAnswerIndex = getRandomInt(0,2)
+
+    // Setja rétta svarið á sinn stað
     answeres[rightAnswerIndex] = ans
 
-    var ques = rand1 + " * " + rand2
-
-    return new Question(ques,answeres,rightAnswerIndex)
+    // Skila Nýrri spurningu
+    return new Question(rand1,rand2,answeres,rightAnswerIndex)
 
 }
 
